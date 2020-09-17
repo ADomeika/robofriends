ad0dd859eeba3c4aa13a2cfa2728873e73217892
Adomas Domeika <adomas.domeika@jet2.com>
2020-09-17 13:33:56 +0100

    Improved accessibility

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Card = ({ item }) => (
   <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-    <img alt='robot profile' src={`https://robohash.org/${item.id}?200x200`} />
+    <img
+      alt='robot profile'
+      src={`https://robohash.org/${item.id}?size=200x200`}
+    />
     <div>
       <h2>{item.name}</h2>
       <p>{item.email}</p>
